4f9f471583a23beb6d034dc554b8ed06e77cb8c6
Ahmed Adel <56580276+Cornato@users.noreply.github.com>
2024-02-11 23:37:07 +0200

    test

---
+++ models/engine/file_storage.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python3
+"""
+FileStorage class definition
+"""
+import json
+from models.base_model import BaseModel
+import os
+
+
+class FileStorage:
+    """FileStorage class for saving instances"""
+
+    __file_path = "file.json"
+    __objects = {}
+
+    def all(self):
+        """Return all stored data"""
+        return self.__objects
+
+    def new(self, obj):
+        """save instance in .__object"""
+        key_for_dic = "{}.{}".format(obj.__class__.__name__, obj.id)
+        self.__objects[key_for_dic] = obj
+
+    def save(self):
+        """Save instance in __objects dictionary"""
+        with open(self.__file_path, "w") as f:
+            data = {}
+            for k, v in self.__objects.items():
+                data[k] = v.to_dict()
+            if len(data) != 0:
+                json.dump(data, f, indent=4)
+
+    def reload(self):
+        """Reload data from .json file"""
+        if os.path.exists(self.__file_path):
+            with open(self.__file_path, "r") as f:
+                restor = json.load(f)
+                for k, v in restor.items():
+                    if '__class__' in v:
+                        cls = v['__class__']
+                        obj = cls(**v)
+                        self.__objects[k] = obj
