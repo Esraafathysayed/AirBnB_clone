7d0c7fa8718039ed34b29260eb5261b0784d6e9b
Ahmed Adel <56580276+Cornato@users.noreply.github.com>
2024-02-12 03:36:50 +0200

    test

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,108 +1,76 @@
-#!/usr/bin/env python3
-"""
-Verifying BaseModel Class
-"""
 import unittest
-import models
-import os
-import uuid
-from io import StringIO
-from datetime import datetime
-from unittest.mock import patch
 from models.base_model import BaseModel
+from io import StringIO
+from unittest import TestCase
+from unittest.mock import patch
+import os
+from datetime import datetime
+from time import sleep
 
+class TestBaseModel(unittest.TestCase):
+    """ Suite to test Base class """
+    
+    """ #1 ATTRIBUTES """
+    
+    def setUp(self):
+        """Set up for all methods"""
+        pass
+    
+    def test_creation(self):
+        """Testing whether is created or not"""
+        Attr122 = BaseModel()
+        self.assertTrue(hasattr(Attr122, "id"))
+        self.assertTrue(hasattr(Attr122, "created_at"))
+        self.assertTrue(hasattr(Attr122, "updated_at"))
 
-class Test_BaseModel(unittest.TestCase):
-    """BaseModel Tests"""
-
-    model_a = BaseModel()
-    model_b = BaseModel()
-
-    def test_init(self):
-        """Verifying __init__(**kwargs)"""
-        model_c = BaseModel(**(self.model_a.to_dict()))
-        self.assertEqual(self.model_a.id, model_c.id)
-        self.assertEqual(self.model_a.created_at, model_c.created_at)
-        self.assertEqual(self.model_a.updated_at, model_c.updated_at)
-        self.assertNotEqual(self.model_a, model_c)
-        self.assertEqual(self.model_a.to_dict(), model_c.to_dict())
-
-    def test_instance(self):
-        """Verifying Is Instance"""
-        self.assertIsInstance(self.model_a, BaseModel)
-        self.assertIsInstance(self.model_b, BaseModel)
-
-    def test_id(self):
-        """Verifying UUID"""
-        self.assertNotEqual(self.model_a.id, self.model_b.id)
-        self.assertEqual(type(self.model_a.id), str)
-        self.assertEqual(type(self.model_b.id), str)
-        self.assertIsInstance(uuid.UUID(self.model_a.id), uuid.UUID)
-
-    def test_datetime(self):
-        """Verifying Time"""
-        self.assertIsInstance(self.model_a.created_at, datetime)
-        self.assertIsInstance(self.model_b.created_at, datetime)
-        self.assertIsInstance(self.model_a.updated_at, datetime)
-        self.assertIsInstance(self.model_b.updated_at, datetime)
-        datetimeiso = self.model_a.created_at.isoformat()
-        from_dict = self.model_a.to_dict()["created_at"]
-        self.assertEqual(datetimeiso, from_dict)
-
-    def test_represent_str(self):
-        """Verifying instance __str__()"""
-        string = self.model_a.__str__()
-        with patch('sys.stdout', new=StringIO()) as dis:
-            print(self.model_a, end="")
-            self.assertEqual(dis.getvalue(), string)
-        string = self.model_b.__str__()
-        with patch('sys.stdout', new=StringIO()) as dis:
-            print(self.model_b, end="")
-            self.assertEqual(dis.getvalue(), string)
-
+    def id_duplications(self):
+        """Is it realy Universal Unique or is it just a name"""
+        id11 = BaseModel()
+        id22 = BaseModel()
+        id33 = BaseModel()
+        id45 = BaseModel()
+        self.assertFalse(id11.id == id22.id)
+        self.assertFalse(id11.id == id33.id)
+        self.assertFalse(id11.id == id45.id)
+        self.assertFalse(id22.id == id33.id)
+        self.assertFalse(id22.id == id45.id)
+        self.assertFalse(id45.id == id45.id)
+        
+    def test_our_types(self):
+        """did we get the string and datetime Right"""
+        Typo2 = BaseModel()
+        self.assertEqual(type(Typo2.id, str))
+        self.assertEqual(type(Typo2.created_at, datetime))
+        self.assertEqual(type(Typo2.updated_at, datetime))
+        
+    """ #2 METHODS """
+    
     def test_save(self):
-        """Verifying instance save()"""
-        t1 = self.model_a.updated_at
-        self.model_a.save()
-        t2 = self.model_b.updated_at
-        self.assertNotEqual(t1, t2)
-        self.assertTrue(os.path.exists("file.json"))
-
-    def test_storage_object(self):
-        """Verifying Storage new method"""
-        restored = models.storage.all()
-        key = "BaseModel.{}".format(self.model_a.id)
-        self.assertEqual(restored[key], self.model_a)
-        key = "BaseModel.{}".format(self.model_b.id)
-        self.assertEqual(restored[key], self.model_b)
-        os.remove("file.json")
-
+        """Do we save the time or we go through black hole"""
+        sa22 = BaseModel()
+        creation_time = sa22.created_at
+        updating_time = sa22.updated_at
+        sleep(0.06)
+        sa22.save()
+        self.assertFalse(updating_time == sa22.updated_at)
+        self.assertTrue(creation_time == sa22.created_at)
+        
     def test_to_dict(self):
-        """Verifying to_dict"""
-        model_dict = self.model_a.__dict__
-        to_dict_ret = self.model_a.to_dict()
-        self.assertNotEqual(model_dict, to_dict_ret)
-        self.assertEqual(to_dict_ret["__class__"],
-                         self.model_a.__class__.__name__)
+        """Is right conerted or not"""
+        the_base22 = BaseModel()
+        dic = the_base22.to_dict()
+        self.assertEqual(dic['__class__'], "BaseModel")
+        self.assertEqual(type(dic['created_at']), str)
+        self.assertEqual(type(dic['updated_at']), str)
+        
+    def test_isoformat(self):
+        """Did we get to ISO 8601"""
+        the_base2 = BaseModel()
+        crtdtime2 = datetime.now()
+        updtdtime2 = datetime.now()
+        the_base2.created_at = crtdtime2
+        the_base2.updated_at = updtdtime2
 
-    def test_addattr(self):
-        """Verifying adding attribute"""
-        self.model_a.some = "some"
-        self.model_a.num = 30
-        self.assertTrue(hasattr(self.model_a, "some"))
-        self.assertTrue(hasattr(self.model_a, "num"))
-        self.assertIsInstance(self.model_a.some, str)
-        self.assertIsInstance(self.model_a.num, int)
-
-    def test_err(self):
-        """Verifying errors"""
-        with self.assertRaises(TypeError):
-            x = self.model_a.to_dict("hii")
-        with self.assertRaises(TypeError):
-            x = self.model_a.save("hii")
-        with self.assertRaises(TypeError):
-            x = self.model_a.__str__("hii")
-
-
-if __name__ == "__main__":
-    unittest.main()
+        dic = the_base2.to_dict()
+        self.assertEqual(dic["created_at"], crtdtime2.isoformat())
+        self.assertEqual(dic["updated_at"], updtdtime2.isoformat())
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,44 +1,117 @@
-#!/usr/bin/env python3
-"""
-Testing FileStorage Class
-"""
-import unittest
-from models.base_model import BaseModel
-from models.engine.file_storage import FileStorage
+#!/usr/bin/python3
+''' Testing our file_storage file '''
+
+
 import os
+import unittest
+from models.engine.file_storage import FileStorage
+from models import storage
+from models.base_model import BaseModel
 
 
-class Test_FileStorage(unittest.TestCase):
-    """Testing FileStorage Class"""
+class TestFileStorage(unittest.TestCase):
+    """Testing the storage object"""
+    
+    def test_01storage_class(self):
+        ''' Testing the storage object '''
+        self.assertIsInstance(storage, FileStorage)
+        
+    """Tests for `__file_path`"""
 
-    model_st = FileStorage()
-    model_bs = BaseModel()
+    def test_02file_path_private(self):
+        
+        with self.assertRaises(AttributeError):
+            storage.file_path
+            storage._file_path
+            storage.__file_path
+        self.assertTrue(hasattr(storage, "_FileStorage__file_path"))
 
-    def test_all(self):
-        """Test all method"""
-        self.assertIsInstance(self.model_st, FileStorage)
-        self.assertIsInstance(self.model_st.all(), dict)
+    def test_03file_path_type(self):
+        
+        self.assertIsInstance(storage._FileStorage__file_path, str)
 
-    def test_new(self):
-        """Test new function"""
-        key = "BaseModel.{}".format(self.model_bs.id)
-        self.model_st.new(self.model_bs)
-        self.assertEqual(self.model_st.all()[key], self.model_bs)
+    def test_04file_path_value(self):
+        
+        self.assertGreater(len(storage._FileStorage__file_path), len(".json"))
+        end_of_file_path = storage._FileStorage__file_path[(-len(".json")):]
+        self.assertEqual(end_of_file_path, ".json")
 
-    def test_save(self):
-        """Testing save"""
-        self.model_st.save()
-        self.assertTrue(os.path.exists("file.json"))
+
+    def test_05objects_private(self):
+        
+        with self.assertRaises(AttributeError):
+            storage.objects
+            storage._objects
+            storage.__objects
+        self.assertTrue(hasattr(storage, "_FileStorage__objects"))
+
+    def test_06objects_type(self):
+        self.assertIsInstance(storage._FileStorage__objects, dict)
+
+# Testing all() method in FileStorage class
+
+    def test_08all_return_type(self):
+        
+        all_objs2 = storage.all()
+        self.assertIsInstance(all_objs2, dict)
+
+    def test_09all_return_value(self):
+        
+        all_objs2 = storage.all()
+        self.assertIs(all_objs2, storage._FileStorage__objects)
+
+    def test_10all_Nof_arguments(self):
+        
+        self.assertRaises(TypeError, storage.all(), "argument")
+
+# Testing new() method in FileStorage class which is used by BaseModel __init__
+
+    def test_11newObj_called_new(self):
+       
+        my_models = BaseModel()
+        my_models.name = "My_First_Model"
+        my_models.my_number = 89
+
+        my_model_key = f"{my_models.__class__.__name__}.{my_models.id}"
+        all_objs = storage.all()
+
+        self.assertIn(my_model_key, all_objs.keys())
+
+        self.assertTrue(hasattr(all_objs[my_model_key], 'id'))
+        self.assertEqual(all_objs[my_model_key].id, my_models.id)
+
+        self.assertTrue(hasattr(all_objs[my_model_key], 'name'))
+        self.assertEqual(all_objs[my_model_key].name, my_models.name)
+
+        self.assertTrue(hasattr(all_objs[my_model_key], 'my_number'))
+        self.assertEqual(all_objs[my_model_key].my_number, my_models.my_number)
+
+
+    def test_12save_return(self):
+        
+        my_models = BaseModel()
+        self.assertIsNone(my_models.save())
+        self.assertIsNone(storage.save())
+
+    def test_13save_Nof_arguments(self):
+        
+        my_model1s = BaseModel()
+        self.assertRaises(TypeError, storage.save(), "argument")
+        self.assertRaises(TypeError, my_model1s.save(), "argument")
+
+    def test_14save_file_exist(self):
+        
+        my_model = BaseModel()
+        my_model.save()
+
+        self.assertTrue(os.path.exists(storage._FileStorage__file_path))
+        self.assertTrue(os.path.getsize(storage._FileStorage__file_path) > 0)
+
+    def tearDownClass(cls):
         os.remove("file.json")
 
-    def test_err(self):
-        """Testing Error"""
-        with self.assertRaises(TypeError):
-            self.model_st.all("no")
-        with self.assertRaises(TypeError):
-            self.model_st.reload("no")
-        with self.assertRaises(TypeError):
-            self.model_st.new("om", "n")
+    def setUpClass(cls):
+        os.remove("file.json")
 
 
 if __name__ == "__main__":
